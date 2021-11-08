46093265ddac77d702b638d86ee94574f53a1d46
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-11-08 09:47:16 +0100

    init

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests;
+
+use  PHPUnit\Framework\TestCase;
+
+
+class ExampleTest extends TestCase {
+
+	public function test_example(
+	) {
+
+		$this->assertEquals(4, 4);
+	}
+
+
+}
+
+
